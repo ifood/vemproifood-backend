35ab3e71d249cc0bb281e47b5b4e602827f20281
bgildson <gildson13@hotmail.com>
2020-04-30 00:06:31 -0300

    setted service properties as private to avoid external access

--- playlists/impl/service.go
+++ playlists/impl/service.go
@@ -6,28 +6,28 @@ import (
 )
 
 type playlistsService struct {
-	TemperatureRepository base.TemperatureRepository
-	PlaylistsRepository   base.PlaylistsRepository
+	temperatureRepository base.TemperatureRepository
+	playlistsRepository   base.PlaylistsRepository
 }
 
 // NewPlaylistsService : factory used to create a PlaylistsService
 func NewPlaylistsService(temperatureRepository base.TemperatureRepository,
 	playlistsRepository base.PlaylistsRepository) base.PlaylistsService {
 	return playlistsService{
-		TemperatureRepository: temperatureRepository,
-		PlaylistsRepository:   playlistsRepository,
+		temperatureRepository: temperatureRepository,
+		playlistsRepository:   playlistsRepository,
 	}
 }
 
 func (s playlistsService) GetPlaylist(city string, latitude float64, longitude float64) (base.Playlist, error) {
-	temperature, err := s.TemperatureRepository.GetTemperature(city, latitude, longitude)
+	temperature, err := s.temperatureRepository.GetTemperature(city, latitude, longitude)
 	if err != nil {
 		return nil, err
 	}
 
 	genre := utils.ParseTemperatureToGenre(temperature)
 
-	playlist, err := s.PlaylistsRepository.GetByGenre(genre)
+	playlist, err := s.playlistsRepository.GetByGenre(genre)
 	if err != nil {
 		return nil, err
 	}
